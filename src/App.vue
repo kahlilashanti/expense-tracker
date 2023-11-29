66e3ccea73afcb35e1be202cfd42d822afd22ec2
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2023-11-29 13:51:02 -0800

    initial build without form

---
+++ src/App.vue
@@ -0,0 +1,57 @@
+<template>
+    <Header />
+    <div class="container">
+        <Balance :total="total"/>
+        <IncomeExpenses :income="income" :expenses="expenses"/>
+        <TransactionList :transactions="transactions"/>
+        <AddTransaction />
+    </div>
+</template>
+
+
+<script setup>
+import Header from './components/Header.vue';
+import Balance from './components/Balance.vue';
+import IncomeExpenses from './components/IncomeExpenses.vue';
+import TransactionList from './components/TransactionList.vue';
+import AddTransaction from './components/AddTransaction.vue';
+
+//to make it reactive use ref
+import { ref, computed } from 'vue';
+//Composition API version
+const transactions= ref([
+                {id: 1, text: 'Flowers', amount: -19.99},
+                {id: 2, text: 'Salary', amount: 209.99},
+                {id: 3, text: 'Book', amount: -12.99},
+                {id: 4, text: 'Squirt Gun', amount: 22 },
+            ]);
+
+
+//get total
+const total = computed(() => {
+    return transactions.value.reduce((acc, transaction) => {
+        return acc + transaction.amount;
+    }, 0);
+});
+
+//get income
+const income = computed(() => {
+    return transactions.value
+    .filter((transaction) => transaction.amount > 0)
+    .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+//get expenses
+const expenses = computed(() => {
+    return transactions.value
+    .filter((transaction) => transaction.amount < 0)
+    .reduce((acc, transaction) => {
+        return acc + transaction.amount;
+    }, 0)
+    .toFixed(2);
+});
+
+</script>
---
+++ src/components/IncomeExpenses.vue
@@ -0,0 +1,30 @@
+
+import { queuePostFlushCb } from 'vue';
+
+<template>
+    <div class="inc-exp-container">
+        <div>
+            <h4>Income</h4>
+            <p id="money-plus" class="money plus">+${{ income }}</p>
+        </div>
+        <div>
+            <h4>Expense</h4>
+            <p id="money-plus" class="money minus">-${{ expenses }}</p>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import {defineProps} from 'vue';
+
+const props = defineProps({
+    income: {
+        Number, 
+        required: true
+    },
+    expenses: {
+        Number, 
+        required: true
+    }
+});
+</script>
