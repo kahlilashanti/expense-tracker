f1738f8b0fa213ab0d8d21640fcf0aad948c529a
Kahlil Ashanti <kahlil.ashanti@gmail.com>
2023-12-14 15:11:42 -0800

    updated vite and removed dist from gitignore

--- vite.config.js
+++ vite.config.js
@@ -13,5 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
-  base: '/expense-tracker/',
+  base: process.env.NODE_ENV === 'production' ? '/expense-tracker/' : '/'
 })
